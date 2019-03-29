c8a543377888b0d96b78442343b9136fb70f4438
connor-mcisaac <connor.mcisaac@outlook.com>
2019-03-29 16:06:58 +0000

    added style sheet

---
+++ PyCBC/style.py
@@ -0,0 +1,55 @@
+# see http://matplotlib.org/users/customizing.html for all options
+
+style1 = {
+    # Line styles
+    'lines.linewidth': 1.0,
+    'lines.antialiased': True,
+
+    # Font
+    'font.size': 16.0,
+
+    # Axes
+    'axes.linewidth': 1.0,
+    'axes.titlesize': 'x-large',
+    'axes.labelsize': 'large',
+    'axes.labelpad': 6.0,
+
+    # Ticks
+    'xtick.major.size': 6,
+    'xtick.minor.size': 4,
+    #'xtick.major.width': 1.0,
+    #'xtick.minor.width': 1.0,
+    'xtick.major.pad': 6.0,
+    'xtick.minor.pad': 6.0,
+    'xtick.labelsize': 'medium',
+    'xtick.direction': 'in',
+    'xtick.top': True,
+
+    'ytick.major.size': 6,
+    'ytick.minor.size': 4,
+    #'ytick.major.width': 1.0,
+    #'ytick.minor.width': 1.0,
+    'ytick.major.pad': 6.0,
+    'ytick.minor.pad': 6.0,
+    'ytick.labelsize': 'medium',
+    'ytick.direction': 'in',
+    'ytick.right': True,
+
+    # Legend
+    'legend.fancybox': True,
+    'legend.fontsize': 'large',
+    'legend.scatterpoints': 5,
+    'legend.loc': 'best',
+
+    # Figure
+    'figure.figsize': [16, 8],
+    'figure.titlesize': 'large',
+
+    # Images
+    'image.cmap': 'viridis',
+    'image.origin': 'lower',
+
+    # Saving
+    'savefig.bbox': 'tight',
+    'savefig.format': 'png',
+}
